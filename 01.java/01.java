fb793eb122f9c8439fc80f4d6221d6b5e2e1ebd1
ArunPragas2001 <pragasarun1@gmail.com>
2025-05-17 10:01:07 +0000

    Update 001

---
+++ 01.java/01.java
@@ -0,0 +1,16 @@
+class Student{
+
+ private int studentId;
+ private String name;
+ private int daysAttended;
+
+
+public student(int studentId, String name, int daysAttended){
+
+this.studentId=studentId;
+this.name=name;
+this.daysAttended=daysAttended;
+
+}
+
+public getName
